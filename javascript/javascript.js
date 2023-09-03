072b8f45557871f5ccaf2c6203c87b60d2eccc93
Zechariah Janowicz <zechariahjanowicz@gmail.com>
2023-09-03 12:53:32 -0500

    added additional javascript so that the calculator functions basic operatives, will add JS so that later the calculator will only ever take in two perameters at a time.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -7,8 +7,10 @@ var x = getNumbers = () => {
 for (i=0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function(event) {
         sum = input.value = input.value + event.currentTarget.value;
-        allNumbers = sum.split(/[-+*]/);     
-        console.log(allNumbers)  
+        allNumbers = sum.split(" "); 
+        if (allNumbers.length = 3) {
+            allNumbers.splice(1, 1);
+        }
         return allNumbers
     })
 }
@@ -19,31 +21,55 @@ for (i=0; i < buttons.length; i++) {
 function multiply() {
     adding = 0;
     multiplying = 1;
+    dividing = 0;
+    subtracting = 0;
 }
 
 function add() {
     adding = 1;
     multiplying = 0;
+    dividing = 0;
+    subtracting = 0;
+}
+function subtract() {
+    subtracting = 1;
+    multiplying = 0;
+    adding = 0;
+    dividing = 0;
 }
 
+function divide() {
+    dividing = 1;
+    multiplying = 0;
+    adding = 0;
+    subtracting = 0;
+}
 
 function total() {
-    if(adding == 1 && multiplying == 0) {
+    if(adding == 1) {
         result = allNumbers.reduce((a, b) => +a + +b);
-        console.log(parseInt(result))
-        console.log(multiplying)
         clear();
     }
     else if(multiplying == 1) {
         result = allNumbers.reduce((a, b) => +a * +b);
-        console.log(parseInt(result))
-        console.log(multiplying)
+        clear();
+    }
+    else if(dividing == 1) {
+        result = parseFloat(allNumbers.reduce((a, b) => +a / +b));
+        clear();
+    }
+    else if (subtracting == 1) {
+        result = allNumbers.reduce((a, b) => +a - +b);
         clear();
     }
 }
 
 function clear() {
     sum = input.value = result
+}
+
+function reset() {
+    sum = input.value = null;
 }
 
 getNumbers();
